06dd3f90de4db5a48681fa6f2f741cb075a99e2f
Konock <bkonock@gmail.com>
2018-08-09 20:55:57 +0200

    Started Bubble exercise

---
+++ week-02/day4/src/greenfox/academy/Bubble.java
@@ -0,0 +1,27 @@
+package greenfox.academy;
+
+public class Bubble {
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as parameter
+    //  Returns a list where the elements are sorted in ascending numerical order
+    //  Make a second boolean parameter, if it's `true` sort that list descending
+
+    //  Example:
+    System.out.println(bubble(new int[]{34, 12, 24, 9, 5}));
+    //  should print [5, 9, 12, 24, 34]
+    // System.out.println(advancedBubble(new int[] {34, 12, 24, 9, 5}, true));
+    //  should print [34, 24, 12, 9, 5]
+  }
+
+  // Bubble
+  // Find the smallest number in the list, put it to the first place
+  // Iterate through list again but from index i+1
+  // Also increase sorted list index
+  // Repeat
+
+  static int[] bubble(int[] numbers) {
+    int temp;
+
+    return numbers;
+  }
+}
