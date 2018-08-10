f5b13b348001d7d656920ac2c4cd2d55a5f62d0d
Konock <bkonock@gmail.com>
2018-08-10 12:16:05 +0200

    Finished LinePlay exercise. +1XP!

--- week-02/day5/src/greenfox/academy/LinePlay.java
+++ week-02/day5/src/greenfox/academy/LinePlay.java
@@ -9,19 +9,37 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlay {
   public static void mainDraw(Graphics graphics) {
-    // This is a set of horizontal and vertical straight(!) lines
-    // with a differing starting point on x and y coordinates
-    // the end points are aligned in a way that it shows a curve
-    //
-    // Create a method for drawing the horizontal and vertical lines
     graphics.setColor(Color.WHITE);
     graphics.fillRect(0, 0, WIDTH, HEIGHT);
+    // Loop for upper part
+    for (int i = 0; i < WIDTH; i += 10) {
+      int x1 = i;
+      int y1 = 0;
+      int x2 = WIDTH;
+      int y2 = i;
+      drawLine(graphics, "purple", x1, y1, x2, y2);
+    }
+    // Loop for bottom part
+    for (int i = 0; i < WIDTH; i += 10) {
+      int x1 = i;
+      int y1 = HEIGHT;
+      int x2 = 0;
+      int y2 = i;
+      drawLine(graphics, "green", x1, y1, x2, y2);
+    }
 
   }
 
-  public static Graphics drawVLine(Graphics graphics, int x, int y) {
-    graphics.setColor(Color.GREEN);
-    graphics.drawLine(x, y, x, y);
+  public static Graphics drawLine(Graphics graphics, String color, int x1, int y1, int x2, int y2) {
+    switch (color) {
+      case "purple":
+        graphics.setColor(new Color(186,85,211));
+        break;
+      case "green":
+        graphics.setColor(Color.GREEN);
+        break;
+    }
+    graphics.drawLine(x1, y1, x2, y2);
     return graphics;
   }
 
