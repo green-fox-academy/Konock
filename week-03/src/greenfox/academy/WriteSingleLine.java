3f9edacc6d695cf0b7f5276bf90bd0ca79a405a5
Konock <bkonock@gmail.com>
2018-08-14 13:26:50 +0200

    started WriteSingleLine exercise

---
+++ week-03/src/greenfox/academy/WriteSingleLine.java
@@ -0,0 +1,19 @@
+package greenfox.academy;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+    // Open a file called "my-file.txt"
+    // Write your name in it as a single line
+    // If the program is unable to write the file,
+    // then it should print an error message like: "Unable to write file: my-file.txt"
+    ArrayList<String> content = new ArrayList<>();
+    content.add("Kinga Bori");
+
+  }
+}
