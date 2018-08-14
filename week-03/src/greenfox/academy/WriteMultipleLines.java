bb6841ff21ed0a99c5d0a77cbfde17fe90b02933
Konock <bkonock@gmail.com>
2018-08-14 13:32:23 +0200

    started Multiplelines exercise

---
+++ week-03/src/greenfox/academy/WriteMultipleLines.java
@@ -0,0 +1,19 @@
+package greenfox.academy;
+
+public class WriteMultipleLines {
+  // Create a function that takes 3 parameters: a path, a word and a number,
+  // than it should write to a file.
+  // The path parameter should be a string, that describes the location of the file.
+  // The word parameter should be a string, that will be written to the file as lines
+  // The number parameter should describe how many lines the file should have.
+  // So if the word is "apple" and the number is 5, than it should write 5 lines
+  // to the file and each line should be "apple"
+  // The function should not raise any error if it could not write the file.
+  public static void main(String[] args) {
+
+  }
+
+  public static void writeLines(String path, String word, int number) {
+
+  }
+}
