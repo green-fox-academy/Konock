a73133d446857c44b4e3e998eba1afcd21c5f523
Konock <bkonock@gmail.com>
2018-08-14 13:30:36 +0200

    finished WriteSingleLine exercise

--- week-03/src/greenfox/academy/WriteSingleLine.java
+++ week-03/src/greenfox/academy/WriteSingleLine.java
@@ -1,10 +1,10 @@
 package greenfox.academy;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.List;
 
 public class WriteSingleLine {
   public static void main(String[] args) {
@@ -14,6 +14,12 @@ public class WriteSingleLine {
     // then it should print an error message like: "Unable to write file: my-file.txt"
     ArrayList<String> content = new ArrayList<>();
     content.add("Kinga Bori");
+    Path filename = Paths.get("my-file.txt");
+    try {
+      Files.write(filename, content);
+    } catch (IOException e) {
+      System.out.println("Unable to write file.");
+    }
 
   }
 }
