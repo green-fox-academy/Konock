70b69c205343101cfbc6bd38137e6649212cdb3f
Konock <bkonock@gmail.com>
2018-08-14 14:48:02 +0200

    added uniqueIP method

--- week-03/src/greenfox/academy/Logs.java
+++ week-03/src/greenfox/academy/Logs.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Logs {
@@ -13,14 +14,26 @@ public class Logs {
   // Write a function that returns the GET / POST request ratio.
   public static void main(String[] args) {
     Path path = Paths.get("log.txt");
+    List<String> content = null;
     try {
-      List<String> content = Files.readAllLines(path);
+      content = Files.readAllLines(path);
     } catch (IOException e) {
       e.printStackTrace();
     }
+    System.out.println(uniqueIP(content));
   }
 
   public static List<String> uniqueIP(List<String> content) {
+    List<String> unique = new ArrayList<>();
+    for (int i = 0; i < content.size(); i++) {
+      String[] line = content.get(i).split(" ");
+      if (!unique.contains(line[8]))
+        unique.add(line[8]);
+    }
+    return unique;
+  }
+
+  public static int ratio(List<String> content) {
 
   }
 }
