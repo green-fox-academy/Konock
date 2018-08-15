9d3fc9935140c3df09cd53c14a3dbd2c4995cd33
Konock <bkonock@gmail.com>
2018-08-15 11:48:00 +0200

    Added Postit exercise

---
+++ week-03/day3/Post-it/src/greenfox/academy/Main.java
@@ -0,0 +1,10 @@
+package greenfox.academy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	      Postit postit1 = new Postit("orange", "Idea1", "blue");
+	      Postit postit2 = new Postit("pink", "Awesome", "black");
+	      Postit postit3 = new Postit("yellow", "Superb", "green");
+    }
+}
---
+++ week-03/day3/Post-it/src/greenfox/academy/Postit.java
@@ -0,0 +1,13 @@
+package greenfox.academy;
+
+public class Postit {
+  private String backgroundColor;
+  private String text;
+  private String textColor;
+
+  Postit(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
