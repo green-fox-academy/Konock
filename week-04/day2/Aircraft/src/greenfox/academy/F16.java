c926d6d9e8326b05c940e3365e15b1af584728fd
Konock <bkonock@gmail.com>
2018-08-21 17:05:37 +0200

    add F16 and F35 classes

---
+++ week-04/day2/Aircraft/src/greenfox/academy/F16.java
@@ -0,0 +1,8 @@
+package greenfox.academy;
+
+public class F16 extends Aircraft {
+  F16() {
+    super.setMaxAmmo(8);
+    super.setBaseDamage(30);
+  }
+}
---
+++ week-04/day2/Aircraft/src/greenfox/academy/F35.java
@@ -0,0 +1,8 @@
+package greenfox.academy;
+
+public class F35 extends Aircraft {
+  F35() {
+    super.setMaxAmmo(12);
+    super.setBaseDamage(50);
+  }
+}
