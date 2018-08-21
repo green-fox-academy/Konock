d2a264437efa94c9f2764fcaa17a715c130894b6
Konock <bkonock@gmail.com>
2018-08-21 11:56:00 +0200

    add Mentor class

---
+++ week-04/day2/src/greenfox/academy/Mentor.java
@@ -0,0 +1,28 @@
+package greenfox.academy;
+
+public class Mentor extends Person {
+  private String level;
+
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + ", a " + getAge() + " year old " + getGender() + ", " + level + " mentor.");
+  }
+
+  Mentor(String name, int age, String gender, String level) {
+    super.setName(name);
+    super.setAge(age);
+    super.setGender(gender);
+    this.level = level;
+  }
+
+  Mentor() {
+    super.setName("Jane Doe");
+    super.setAge(30);
+    super.setGender("female");
+    level = "intermediate";
+  }
+
+}
