2667aaf5fb09a68d0979f58944f26730ead3dead
Konock <bkonock@gmail.com>
2018-08-22 13:34:34 +0200

    initialize Comparable project copy Domino task

---
+++ week-04/day3/Comparable/src/greenfox/academy/Main.java
@@ -0,0 +1,40 @@
+package greenfox.academy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Domino> dominoes = initializeDominoes();
+        // You have the list of Dominoes
+        // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+        // eg: [2, 4], [4, 3], [3, 5] ...
+
+        System.out.println(dominoes);
+        System.out.println(sortDominoes(dominoes));
+    }
+
+    static List<Domino> initializeDominoes() {
+        List<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+        return dominoes;
+    }
+
+    static List<Domino> sortDominoes(List<Domino> dominoes) {
+        List<Domino> sortedDominoes = new ArrayList<>();
+        sortedDominoes.add(dominoes.get(0));
+        while (sortedDominoes.size() != dominoes.size()) {
+            for (int i = 1; i < dominoes.size(); i++) {
+                if (sortedDominoes.get(sortedDominoes.size() - 1).getValues()[1] == dominoes.get(i).getValues()[0]) {
+                    sortedDominoes.add(dominoes.get(i));
+                }
+            }
+        }
+        return sortedDominoes;
+    }
+}
