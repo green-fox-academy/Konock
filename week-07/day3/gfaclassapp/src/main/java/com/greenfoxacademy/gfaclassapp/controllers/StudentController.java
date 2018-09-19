e6ec2f094600f40f57226ebd667b2d8386ba6005
Konock <bkonock@gmail.com>
2018-09-19 23:42:18 +0200

    add checkStudent function to app

--- week-07/day3/gfaclassapp/src/main/java/com/greenfoxacademy/gfaclassapp/controllers/StudentController.java
+++ week-07/day3/gfaclassapp/src/main/java/com/greenfoxacademy/gfaclassapp/controllers/StudentController.java
@@ -4,10 +4,7 @@ import com.greenfoxacademy.gfaclassapp.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -39,6 +36,17 @@ public class StudentController {
   public String addStudent(@ModelAttribute(value = "student-name") String name) {
     studentService.save(name);
     return "redirect:/list";
+  }
+
+  @GetMapping("/check")
+  public String checkPage() {
+    return "check";
+  }
+
+  @GetMapping("/checkstudent")
+  public String checkStudent(Model model, @RequestParam("student-name") String name) {
+    model.addAttribute("ifPresent", studentService.checkIfPresent(name));
+    return "checkstudent";
   }
 
 }
--- week-07/day3/gfaclassapp/src/main/java/com/greenfoxacademy/gfaclassapp/services/StudentService.java
+++ week-07/day3/gfaclassapp/src/main/java/com/greenfoxacademy/gfaclassapp/services/StudentService.java
@@ -27,4 +27,8 @@ public class StudentService {
   public int count() {
     return names.size();
   }
+
+  public boolean checkIfPresent(String name) {
+    return names.contains(name);
+  }
 }
