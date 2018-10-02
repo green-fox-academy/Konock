4fb7052cd7f417d8e50477ae8ca635cd95d749f2
Konock <bkonock@gmail.com>
2018-10-02 14:44:27 +0200

    test(GuardianControllerTest): initialize test class

---
+++ week-09/day2/groot/src/test/java/com/greenfoxacademy/groot/GuardianControllerTest.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.groot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GuardianController.class)
+public class GuardianControllerTest {
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+      MediaType.APPLICATION_JSON.getSubtype(),
+      Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void grootShouldReturnIAmGroot_when_inputIsSomemessage() throws Exception {
+
+  }
+}
