c84ed20041083b15fef1e6cd1936a47a3f22fc4d
Konock <bkonock@gmail.com>
2018-10-04 14:02:50 +0200

    chore(Spaceship App): initialize project

---
+++ week-09/day2/groot/src/main/java/com/greenfoxacademy/groot/GrootApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.groot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrootApplication.class, args);
+  }
+}
---
+++ week-09/day4/corsac-orientation-exam/src/main/java/com/greenfoxacademy/corsacorientationexam/CorsacOrientationExamApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.corsacorientationexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsacOrientationExamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CorsacOrientationExamApplication.class, args);
+  }
+}
